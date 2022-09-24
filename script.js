9e35f7efe53db733fa111df852bf59621be28ea1
Abdur Rahman IsHa <74505859+abdurrahmanisha1@users.noreply.github.com>
2022-09-24 13:48:47 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Header Section 
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+// Menu toggle 
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
